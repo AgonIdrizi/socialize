0415f47083c1ef246567215ca977e545038b2aac
AgonIdrizi <agon.88@hotmail.com>
2018-10-15 19:29:37 +0200

    Ajax requests for likes add

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,33 @@
 class LikesController < ApplicationController
   def index
   	@post = Post.find_by(id: params[:post_id])
-  	@likes = @post.likes
+  	
   end
 
   def create
   	@post = Post.find_by(id: params[:post_id])
-  	if @post.likes.create(name: "#{current_user.name}", user_id: current_user.id)
-  	  redirect_to posts_path
+    @like = @post.likes.create(name: "#{current_user.name}", user_id: current_user.id)
+    #debugger
+  	if @like.save
+  	  respond_to do |format|
+      format.html {redirect_to root_path}
+      format.js 
+      end
   	else
+      redirect_to root_path
   	end
+  end
+
+  def destroy
+    @post = Post.find_by(id: params[:post_id])
+    @like = @post.likes.find_by(user_id: current_user.id)
+    if @like.destroy
+      respond_to do |format|
+        format.html {redirect_to root_path}
+        format.js 
+      end
+    else
+      redirect_to root_path
+    end
   end
 end
