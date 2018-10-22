eea15723ccca8e458b8198a57869d994b769381a
AgonIdrizi <agon.88@hotmail.com>
2018-10-22 20:14:07 +0200

    callbackscontroller change

--- app/controllers/auth/callbacks_controller.rb
+++ app/controllers/auth/callbacks_controller.rb
@@ -1,15 +1,9 @@
 class Auth::CallbacksController < ApplicationController
   
   def facebook
-      @user = User.create_from_provider_data(request.env['omniauth.auth'])
-      if @user.persisted?
-        sign_in_and_redirect @user
-        set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
-      else
-        flash[:error] = 'There was a problem signing you in through Facebook. Please register or try signing in later.'
-        redirect_to new_user_registration_url
-      end 
-    end
+    user = User.create_from_provider_data(request.env['omniauth.auth'])
+    sign_in_and_redirect user, event: :authentication if user.persisted? 
+  end
 
   def failure
   	flash[:error] = 'There was a problem signing you in. Please register or try signing in later.' 
