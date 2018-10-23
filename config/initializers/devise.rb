0f00331f0a181b437f4bb1fa50863760e11221ca
AgonIdrizi <agon.88@hotmail.com>
2018-10-23 14:31:38 +0200

    omniatu.config facebook change

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -257,7 +257,10 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :facebook, ENV['FB_APP_ID'], ENV['FB_APP_SECRET'], secure_image_url: true
+  config.omniauth :facebook, ENV['FB_APP_ID'], ENV['FB_APP_SECRET'], scope: 'public_profile,email, user_about_me',
+                                                                    secure_image_url: true,
+                                                                    image_size: :large,
+                                                                    info_fields: 'name,first_name,last_name,email'
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
