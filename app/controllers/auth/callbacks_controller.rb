6f2b5cec78f364a5e42633c042b011dcfa8b9a2a
AgonIdrizi <agon.88@hotmail.com>
2018-10-23 15:45:29 +0200

    typo fix

--- app/controllers/auth/callbacks_controller.rb
+++ app/controllers/auth/callbacks_controller.rb
@@ -1,7 +1,6 @@
 class Auth::CallbacksController < ApplicationController
   
   def facebook
-    User.set_param(request.env['omniauth.auth'])
     user = User.from_omniauth(request.env['omniauth.auth'])
     sign_in_and_redirect user, event: :authentication if user.persisted?
   end
