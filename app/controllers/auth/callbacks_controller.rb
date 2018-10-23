c069a8240ee6740ceaff1a0fccabb506fdc89c99
AgonIdrizi <agon.88@hotmail.com>
2018-10-23 14:10:47 +0200

    user image from omniauth add

--- app/controllers/auth/callbacks_controller.rb
+++ app/controllers/auth/callbacks_controller.rb
@@ -2,9 +2,8 @@ class Auth::CallbacksController < ApplicationController
   
   def facebook
     user = User.from_omniauth(request.env['omniauth.auth'])
-    sign_in_and_redirect user  
+    sign_in_and_redirect user, event: :authentication if user.persisted?
   end
 
-  
  
 end
