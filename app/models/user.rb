8d891e45077e1db3ad44b7160ca9b0bebd40896f
AgonIdrizi <agon.88@hotmail.com>
2018-10-23 14:22:50 +0200

    omniauth method change

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,7 +28,7 @@ class User < ApplicationRecord
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
-      user.image = auth.info.image
+      user.image = URI.parse(auth.info.image)
     end
   end
 
