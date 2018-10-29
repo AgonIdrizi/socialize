4c5c53403ba2744f0112289e4ae8b938c1a3a287
AgonIdrizi <agon.88@hotmail.com>
2018-10-29 19:55:06 +0100

    added image to post

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,6 +3,6 @@ module UsersHelper
   def show_all_posts_of_friends
   	friends = current_user.friends
   	friends << current_user if display_posts_of_current_user
-	friends.includes(posts: [{comments: :user}],image_attachment: [:blob]).order(created_at: :ASC)
+	friends.includes(posts: [{comments: :user},{image_attachment: :blob}],image_attachment: [:blob]).order(created_at: :ASC)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
 	belongs_to :user, optional: true
 	has_many :comments, as: :commentable
 	has_many :likes, as: :likeable
-
+	has_one_attached :image
 	validates :user_id, presence: true
 
 	
